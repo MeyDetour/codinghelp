cdd09b675bdff1088bdb4f85c39a46ea83339f3c
mey <meydetour@gmail.com>
2024-12-12 11:45:27 +0100

    edit cors

--- coding/settings.py
+++ coding/settings.py
@@ -109,11 +109,11 @@ WSGI_APPLICATION = 'coding.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('DB_NAME'),
-        "USER": os.getenv('DB_USER'),
-        "PASSWORD": os.getenv('DB_PASSWORD'),
-        "HOST": os.getenv('DB_HOST'),
-        "PORT": os.getenv('DB_PORT'),
+        'NAME': 'codinghelp',
+        "USER":"mey",
+        "PASSWORD": "rVOG23VhmpGLQF4c",
+        "HOST": "localhost",
+        "PORT": 5432,
     }
 }
 
